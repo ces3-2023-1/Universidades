27cbb3bc577ebd445d19effde62043a6b63ebb88
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-03-16 23:21:41 -0500

    feat: elian

--- src/main/java/com/edu/poi/ces3/main.java
+++ src/main/java/com/edu/poi/ces3/main.java
@@ -1,4 +1,15 @@
 package com.edu.poi.ces3;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
 public class main {
+    public static void main(String[] args) {
+        try {
+            ObjectMapper mapper = new XmlMapper();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
