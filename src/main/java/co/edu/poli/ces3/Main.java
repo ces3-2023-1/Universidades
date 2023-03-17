0d83a03b97f1dfea4ba0ff99c6c4f6adc2900919
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-03-16 23:07:33 -0500

    feat: subido por elian ruiz

--- src/main/java/co/edu/poli/ces3/Main.java
+++ src/main/java/co/edu/poli/ces3/Main.java
@@ -7,11 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /*
+        
         try {
             ObjectMapper mapper = new XmlMapper();
         }catch (Exception e){
             System.out.println(e.getMessage());
-        }*/
+        }
     }
 }
