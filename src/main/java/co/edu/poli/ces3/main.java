93d39993b9090c672f3a52fa17d47fbc051f012c
Santiago Cortes <scortes@quadfocus.com>
2023-04-07 19:29:31 -0500

    Imprimiendo la estructura de datos en el proyecto AutoFact

--- src/main/java/co/edu/poli/ces3/main.java
+++ src/main/java/co/edu/poli/ces3/main.java
@@ -2,7 +2,9 @@ package co.edu.poli.ces3;
 
 import co.edu.poli.ces3.models.Estudiantes;
 import co.edu.poli.ces3.models.Materias;
+import co.edu.poli.ces3.models.autofact.Distribuidores;
 import co.edu.poli.ces3.models.autofact.Prefacturas;
+import co.edu.poli.ces3.models.autofact.Productos;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
@@ -19,39 +21,36 @@ public class main {
         try
         {
             ObjectMapper mapper = new XmlMapper();
-            //InputStream--> Permite parte nativa java, captura de datos. Entradas y salidas de datos
-            //FileInputStream --> declara un parametro que es un archivo, una imagen y una cadena string
             InputStream inputStream = new FileInputStream(new File("src\\resources\\Prefacturas.xml"));
             TypeReference<List<Prefacturas>> typeReference = new TypeReference<List<Prefacturas>>(){};
             List<Prefacturas> prefacturas = mapper.readValue(inputStream, typeReference);
 
+            System.out.println("                Reportes Prefacturas Don Churro S.A.S                                    ");
             for (Prefacturas x:prefacturas) {
-                System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++");
+                System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
                 System.out.println("Nombre: "+ x.getNombre());
                 System.out.println("Fecha Subida: "+ x.getFechaSubida());
                 System.out.println("Resposable De: "+ x.getAudit_createBy());
-                System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++");
-                /*for (Materias mat:x.getMaterias()) {
-                    System.out.println("Nom Materia: "+ mat.getNombreMat());
-                    System.out.println("Codigo Registro: "+ mat.getCodeRegistro());
-                    System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++");
-                }*/
-                System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++");
-            }
 
-         /*   for (Estudiantes x:estudiantes) {
-                System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++");
-                System.out.println("Estudiante: "+ x.getNombre());
-                System.out.println("Comentario: "+ x.getComentario());
-                System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++");
-                System.out.println("Materias");
-                for (Materias mat:x.getMaterias()) {
-                    System.out.println("Nom Materia: "+ mat.getNombreMat());
-                    System.out.println("Codigo Registro: "+ mat.getCodeRegistro());
-                    System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++");
+                System.out.println("                Distribuidores          ");
+                for(Distribuidores dist:x.getDistribuidores()) {
+                    System.out.println("Nom. Distribuidor: "+ dist.getNombre());
+                    System.out.println("Codigo Distribuidor: "+ dist.getCodigo());
+                    System.out.println("                Productos           ");
+
+                    for (Productos prod:dist.getProductos()) {
+                        System.out.println("Cod. Producto: "+ prod.getCode());
+                        System.out.println("Nombre Producto: "+prod.getNombre());
+                        System.out.println("Cantidad: "+ prod.getCantidad());
+                        System.out.println("Valor: $"+ prod.getTotalNeto());
+                        System.out.println("IVA: "+ prod.getIva() * 100 + "%");
+                        System.out.println("Valor Total: $" + (prod.getTotalNeto() * prod.getCantidad()) * prod.getIva());
+                        System.out.println("Descripcion: "+ prod.getDescripcion());
+                        System.out.println("Estado: "+ prod.getProceso());
+                    }
                 }
                 System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++");
-            }*/
+            }
         }
         catch(IOException ex)
         {
