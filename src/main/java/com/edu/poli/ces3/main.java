d5d33016e589705e9e56386c72334442a1594f6c
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-04-13 21:43:10 -0500

    Agregado servicios mediante xml del proyecto ppi

--- src/main/java/com/edu/poli/ces3/main.java
+++ src/main/java/com/edu/poli/ces3/main.java
@@ -1,7 +1,6 @@
 package com.edu.poli.ces3;
 
-import com.edu.poli.ces3.models.Matter;
-import com.edu.poli.ces3.models.Student;
+import com.edu.poli.ces3.models.Servicios;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
@@ -15,26 +14,32 @@ public class main {
     public static void main(String[] args) {
         try {
             ObjectMapper mapper = new XmlMapper();
-            InputStream inputStream = new FileInputStream(new File("src\\resourses\\Estudiante.xml"));
-            //System.out.println(System.getProperty("user.dir"));
-            TypeReference<List<Student>> typeReference = new TypeReference<List<Student>>() {};
-            List<Student> students = mapper.readValue(inputStream, typeReference);
+            InputStream inputStream = new FileInputStream(new File("src\\resourses\\Servicio.xml"));
+            TypeReference<List<Servicios>> typeReference = new TypeReference<List<Servicios>>() {};
+            List<Servicios> students = mapper.readValue(inputStream, typeReference);
 
-            for (Student s: students) {
-                System.out.println("----- Informacion estudiante -----");
-                System.out.println("Nombre: " + s.getNombre() +", edad: " + s.getEdad() + ", comentario: " + s.getComentario() + ".");
-                System.out.println("----- Listado de materias -----");
-                for (Matter m: s.getMaterias()){
-                    System.out.println("Cod. Registro: " + m.getCodReg() +", codigo: " + m.getCodigo() + ", nombre de la materia: " + m.getNombreMat() + ", estado: " + m.getEstado() + ".");
+            for (Servicios s: students) {
+                System.out.println("----- Informacion del Servicio -----");
+                System.out.println(" ");
+                if(s.getActivo() == false ){
+                    System.out.println("El servicio: " +s.getNombre() +", con un valor de: $"+s.getValor() +" pesos, No esta activo.");
+                }else{
+                    System.out.println("El servicio: " + s.getNombre() +", con un valor de: $" + s.getValor() + " pesos, Si esta activo.");
+                }
+                if(s.getValor() >= 30000){
+                    System.out.println("Servicio con costo superior a 30 mil pesos: " + s.getNombre());
+                    System.out.println(" ");
+
+                }else{
+                    System.out.println("Servicio con costo inferior a 30 mil pesos: " + s.getNombre());
+                    System.out.println(" ");
 
                 }
-
             }
 
-            System.out.println("cool!!");
 
         }catch (Exception e){
-            System.out.println("mi error: " + e.getMessage());
+            System.out.println(e.getMessage());
 
         }
 
---
+++ src/main/java/com/edu/poli/ces3/models/Servicios.java
@@ -0,0 +1,33 @@
+package com.edu.poli.ces3.models;
+
+import java.util.List;
+
+public class Servicios {
+    String nombre;
+    int valor;
+    Boolean activo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+}
